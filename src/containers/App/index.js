dee645bbc6cc5e7fb94bc1e7e5ad0266d6c9bf47
DongNguyen <nguyenthiendong17031998@gmail.com>
2021-09-06 17:17:38 +0700

    Tich hop redux, reduxjs-toolkit

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -3,13 +3,17 @@ import React, { Component } from 'react';
 import theme from '../../commons/Theme';
 import Taskboard from '../Taskboard';
 import styles from './styles';
+import { Provider } from 'react-redux';
+import store from '../../redux/index';
 
 class App extends Component {
   render() {
     return (
-      <ThemeProvider theme={theme}>
-        <Taskboard />
-      </ThemeProvider>
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <Taskboard />
+        </ThemeProvider>
+      </Provider>
     );
   }
 }
