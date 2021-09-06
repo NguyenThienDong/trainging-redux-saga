ddbbe092b8b9308415328b2251aa6a388a4a8d09
DongNguyen <nguyenthiendong17031998@gmail.com>
2021-09-07 02:49:39 +0700

    Redux saga - Take and Call

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,8 +1,18 @@
-import { fork } from 'redux-saga/effects';
+import { call, fork, take } from 'redux-saga/effects';
+import * as taskConstants from '../constants/task';
+import { getList } from '../apis/task';
+import { STATUS_CODE } from '../constants/index';
 
 function* watchFetchListTaskAction() {
-  yield true;
-  console.log('watching fetch list task action');
+  yield take(taskConstants.FETCH_TASKS);
+  const resp = yield call(getList);
+  console.log(resp);
+  const { status } = resp;
+  if (status === STATUS_CODE.SUCCESS) {
+    // dispatch action fetchListTaskSuccess
+  } else {
+    // dispatch action fetchListTaskFail
+  }
 }
 
 function* watchCreateTaskAction() {
