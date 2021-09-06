d4fe599869149ee1c3a31bc01f16b3c4b99b0049
DongNguyen <nguyenthiendong17031998@gmail.com>
2021-09-06 17:51:33 +0700

    Axios and create Base server

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 import {combineReducers} from '@reduxjs/toolkit';
+import taskReducer from './task';
 
 const rootReducer = combineReducers({
-
+  task: taskReducer
 })
 
 export default rootReducer;
