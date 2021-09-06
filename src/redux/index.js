3695a861942ee83f426033556487983768d4e3c6
DongNguyen <nguyenthiendong17031998@gmail.com>
2021-09-07 00:24:11 +0700

    Tich hop redux-saga

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,8 +1,16 @@
-import { configureStore } from '@reduxjs/toolkit';
+import createSagaMiddleware from 'redux-saga';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import rootReducer from '../reducers';
+import rootSaga from '../sagas';
+
+const sagaMiddleware = createSagaMiddleware();
+const middleware = [...getDefaultMiddleware(), sagaMiddleware];
 
 const store = configureStore({
   reducer: rootReducer,
+  middleware,
 });
+
+sagaMiddleware.run(rootSaga);
 
 export default store;
---
+++ src/sagas/index.js
@@ -0,0 +1,6 @@
+function* rootSaga() {
+  yield true;
+  console.log('Day la root saga');
+}
+
+export default rootSaga;
