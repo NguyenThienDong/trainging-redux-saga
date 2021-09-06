7505cd9e78e25c6485319eb5b27fccd354e1cf15
DongNguyen <nguyenthiendong17031998@gmail.com>
2021-09-07 02:21:58 +0700

    Redux saga - Fork

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,6 +1,18 @@
-function* rootSaga() {
+import { fork } from 'redux-saga/effects';
+
+function* watchFetchListTaskAction() {
   yield true;
-  console.log('Day la root saga');
+  console.log('watching fetch list task action');
+}
+
+function* watchCreateTaskAction() {
+  yield true;
+  console.log('watching create task action');
+}
+
+function* rootSaga() {
+  yield fork(watchFetchListTaskAction);
+  yield fork(watchCreateTaskAction);
 }
 
 export default rootSaga;
